b066a8e425ed3e116082e310348d1a564ae9c8a0
he-who-must-not-be-nam3d <wanjurisamuel@gmail.com>
2024-11-15 10:44:15 +0300

    Successfully inserted data into the db through the form

--- sacco/app_forms.py
+++ sacco/app_forms.py
@@ -7,3 +7,6 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name','email', 'gender', 'dob', 'weight']
+        widgets = {
+            'dob' : forms.DateInput(attrs={'class': 'datepicker', 'type': 'date'})
+        }
